34ed67113f5b471335d6587ca8e6cabdebdf5903
GabrielHenri12 <gabrielhenri2002@gmail.com>
2022-12-15 14:15:54 -0300

    modifications in server

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,24 +1,6 @@
-import express, { Request, Response }  from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-import cors from 'cors'
-import apiRoutes from './routes/api'
- 
-dotenv.config();
+import App from "./App";
+import dotenv from "dotenv";
 
-const server = express();
+dotenv.config()
 
-server.use(cors());
-
-server.use(express.json())
-server.use(express.static(path.join(__dirname, '../public')));
-server.use(express.urlencoded({extended: true}));
-
-server.use(apiRoutes);
-
-server.use((req:Request, res:Response)=>{
-    res.status(404);
-    res.json({error:'Endpoint não encontrado'})
-})
-
-server.listen(process.env.PORT);
+App.listen(process.env.PORT);
