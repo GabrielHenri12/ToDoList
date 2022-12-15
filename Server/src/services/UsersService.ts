a6d4fa47a0032ea38f86f50ec8880a0c78a43580
GabrielHenri12 <gabrielhenri2002@gmail.com>
2022-12-15 17:13:11 -0300

    create setToken

--- Server/src/services/UsersService.ts
+++ Server/src/services/UsersService.ts
@@ -1,14 +1,18 @@
 import { Users } from "../models/User";
 import bcrypt from "bcrypt"
+import { generateToken } from "../configs/passport";
 
 export const CreateUser = async (name: string, email: string, password: string) => {
+    
     const hasUser = await Users.findOne({ where: { email } })
     if (!hasUser) {
         const hash = bcrypt.hashSync(password, 10)
+        const token = generateToken({email})
         const newUser = await Users.create({
             name,
             email,
-            password: hash
+            password: hash,
+            token
         })
         return newUser
     } else {
@@ -18,6 +22,16 @@ export const CreateUser = async (name: string, email: string, password: string) 
 
 export const FindByEmail = async (email: string) => {
     return await Users.findOne({ where: { email } })
+}
+
+export const setToken = async (email: string) => {
+    let user = await FindByEmail(email)
+    if(user){
+        let newToken = generateToken({email})
+        user.token = newToken
+        user.save()
+        return newToken
+    }
 }
 
 export const matchPassword = async (PasswordText: string, encripted: string) => {
