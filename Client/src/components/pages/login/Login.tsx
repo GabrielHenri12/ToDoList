699f0b7722569d431787e6ec5c8a7d4410e7ec12
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-01-17 15:37:00 -0300

    creat page and configs of login

--- Client/src/components/pages/login/Login.tsx
+++ Client/src/components/pages/login/Login.tsx
@@ -1,20 +1,57 @@
-import { Button, Center, color, Heading, Stack } from "@chakra-ui/react"
-import Inputs from "../../comuns/Inputs"
-import { useState } from "react"
+import { Button, Center, Heading, Link, Stack } from "@chakra-ui/react"
 import InputPassword from "../../comuns/InputPassword"
+import Inputs from "../../comuns/Inputs"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import api from "../../../API/Api"
+import { signIn } from "../../../API/auth"
 
 export default () => {
+    const [password, setPassword] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [Error, setError] = useState<string>('')
+    const [Loading, setLoading] = useState<boolean>(false)
+    const natigate = useNavigate()
+
+    function login() {
+        setLoading(true);
+        if (email === '') { return setError('Preencha todos os campos') }
+        if (password === '') { return setError('Preencha todos os campos') }
+        api
+            .post('/login', { email, password })
+            .then(response => {
+                setLoading(false);
+                console.log(response.data.token)
+                signIn(response.data.token)
+                natigate('/tarefas')
+            })
+            .catch(error => { setLoading(false); console.log(error) })
+    }
+
     return (
-        <Center flexDirection='column' justifyContent='space-around' border={['0px', '1px', '1px']} borderRadius='10px' borderColor='gray' w={['100vw', '60vw', '30vw']} h={['100vh', '80vh', '70vh']} bg='cyan.500'>
-            <Heading as='h1' size='lg'>Entrar</Heading>
+        <Center flexDirection='column' justifyContent='space-around' border={['0px', '1px', '1px']} borderRadius='10px' borderColor='gray' w={['100vw', '60vw', '30vw']} h={['100vh', '80vh', '70vh']} bg='blue.200'>
+            <Heading as='h1' size='lg' color='white'>Entrar</Heading>
+            {Error}
             <Stack>
-                <Inputs />
-                <InputPassword />
+                <Inputs
+                    placeholder={'Email'}
+                    value={email}
+                    setFunc={setEmail}
+                />
+                <InputPassword
+                    placeholder={'Senha'}
+                    value={password}
+                    setFunc={setPassword}
+                />
+                <Link href="/cadastro" textAlign='center' color='white'>criar conta<ExternalLinkIcon mx='2px' /></Link>
             </Stack>
             <Button
-                isLoading={false}
+                isLoading={Loading}
+                onClick={login}
                 loadingText='Loading'
-                colorScheme='whatsapp'
+                bg='blue.100'
+                color="white"
                 spinnerPlacement='start'
             >
                 Entrar
