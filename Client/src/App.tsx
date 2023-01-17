60181b007d590ef2d92f16032d19f26594a4e908
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-01-17 15:34:18 -0300

    add Page NotFound and corrections

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,16 +1,19 @@
 import { Center } from '@chakra-ui/react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { isAuthenticated } from './API/auth'
+import NotFound from './components/pages/404/NotFound'
 import Login from './components/pages/login/Login'
 import Register from './components/pages/register/Register'
+import Tasks from './components/pages/tasks/Tasks'
 
 function App() {
   return (
     <BrowserRouter>
-      <Center w={'100vw'} h={'100vh'} bg='blue.100'>
+      <Center w={'100vw'} h={'100vh'} bg='gray.100' overflow='hidden'>
         <Routes>
-          <Route path='/Entrar' element={<Login />} />
+          <Route path='/' element={<Login />} />
           <Route path='/Cadastro' element={<Register />} />
+          <Route path='/tarefas' element={<Tasks/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
       </Center>
     </BrowserRouter>
---
+++ Client/src/components/pages/404/NotFound.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "@chakra-ui/react"
+
+export default () => {
+    return (
+        <Heading>
+            Página não encontrada!
+        </Heading>
+    )
+}
