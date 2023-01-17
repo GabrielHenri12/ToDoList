2ad6f53169009ce636398411c8c4e4fe6765963c
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-01-17 15:37:14 -0300

    creat page and configs register

--- Client/src/components/pages/register/Register.tsx
+++ Client/src/components/pages/register/Register.tsx
@@ -1,21 +1,55 @@
+import { Button, Center, Link, Heading, Stack } from "@chakra-ui/react";
 import InputPassword from "../../comuns/InputPassword";
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { useNavigate } from "react-router-dom";
 import Inputs from "../../comuns/Inputs";
-import { Button, Center, Heading, Stack } from "@chakra-ui/react";
+import { useState } from "react";
+import api from "../../../API/Api";
+import { signIn } from "../../../API/auth";
 
 export default () => {
+    const [password, setPassword] = useState<string>('')
+    const [ConfirmPassword, setConfirmPassword] = useState<string>('')
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [Error, setError] = useState<string>('')
+    const [Loading, setLoading] = useState<boolean>(false)
+    const natigate = useNavigate()
+
+    function register() {
+        setLoading(true);
+        if (password != ConfirmPassword) { return setError('Senhas dífrenentes') }
+        if (email === '') { return setError('Preencha todos os campos') }
+        if (name === '') { return setError('Preencha todos os campos') }
+        if (password === '') { return setError('Preencha todos os campos') }
+        api
+            .post('/register', { name, email, password })
+            .then(response => {
+                setLoading(false);
+                console.log(response.data)
+                signIn(response.data.newUser)
+                natigate('/')
+            })
+            .catch(error => { setLoading(false); console.log(error) })
+    }
+
     return (
-        <Center flexDirection='column' justifyContent='space-around' border={['0px', '1px', '1px']} borderRadius='10px' borderColor='gray' w={['100vw', '60vw', '30vw']} h={['100vh', '80vh', '70vh']} bg='cyan.500'>
-            <Heading as='h1' size='lg'>Cadastre-se</Heading>
+        <Center flexDirection='column' justifyContent='space-around' border={['0px', '1px']} borderRadius='5px' w={['100vw', '60vw', '30vw']} h={['100vh', '80vh', '70vh']} bg='blue.200'>
+            <Heading as='h1' size='lg' color="white">Cadastre-se</Heading>
+            {Error}
             <Stack>
-                <Inputs/>
-                <Inputs/>
-                <InputPassword/>
-                <InputPassword/>
+                <Inputs placeholder={'Nome'} value={name} setFunc={setName} />
+                <Inputs placeholder={'Email'} value={email} setFunc={setEmail} />
+                <InputPassword placeholder={'Senha'} value={password} setFunc={setPassword} />
+                <InputPassword placeholder={'Confirme a senha'} value={ConfirmPassword} setFunc={setConfirmPassword} />
+                <Link href="/entrar" textAlign='center' color='white'>Já tem conta?<ExternalLinkIcon mx='2px' /></Link>
             </Stack>
             <Button
-                isLoading={false}
+                isLoading={Loading}
+                onClick={register}
                 loadingText='Loading'
-                colorScheme='whatsapp'
+                bg='blue.100'
+                color='white'
                 spinnerPlacement='start'
             >
                 Cadastrar
