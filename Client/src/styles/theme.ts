5f7b41b5a09d72d179b978f5f3b6da659d58b71f
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-01-17 15:38:20 -0300

    styles configs ScrollBar

--- Client/src/styles/theme.ts
+++ Client/src/styles/theme.ts
@@ -1,6 +1,18 @@
 import { extendTheme } from '@chakra-ui/react'
 
-const custonTheme = {};
+const custonTheme = {
+    colors: {
+        blue: {
+            100: '#00BBC9',
+            200: '#00747C',
+        },
+        gray: {
+            100: '#CACACA',
+            200: '#878787',
+            300: '#202022',
+        }
+    }
+}
 
 const theme = extendTheme(custonTheme);
 
